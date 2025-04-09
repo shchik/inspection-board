60eba14d32f63ed14edba656aa768a9de71e921c
shchik <nikshc2005@mail.ru>
2025-04-09 15:14:33 +0300

    feat: create loader component

--- client/src/app/components/faculty-list/faculty-list.component.ts
+++ client/src/app/components/faculty-list/faculty-list.component.ts
@@ -1,16 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { catchError } from 'rxjs';
 import { IFaculty } from '../../model/faculty.type';
 import { FacultyService } from '../../services/faculty.service';
 import { FacultyCardComponent } from '../faculty-card/faculty-card.component';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-faculty-list',
-  imports: [FacultyCardComponent],
+  imports: [FacultyCardComponent, NgIf, LoaderComponent],
   templateUrl: './faculty-list.component.html',
   styleUrl: './faculty-list.component.scss',
 })
 export class FacultyListComponent implements OnInit {
+  loader = signal<boolean>(true);
   facultyService = inject(FacultyService);
   facultyItems = signal<IFaculty[]>([]);
 
@@ -19,12 +22,13 @@ export class FacultyListComponent implements OnInit {
       .getFaculties()
       .pipe(
         catchError((err) => {
-          console.log(err);
-          throw new err();
+          console.error(err);
+          throw err;
         })
       )
       .subscribe((faculties) => {
         this.facultyItems.set(faculties);
+        this.loader.set(false);
       });
   }
 }
